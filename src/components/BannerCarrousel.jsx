e48fb51c42e0290905fa0732742b1154ab6d027d
YoannPitance <pittyo88@gmail.com>
2023-03-14 18:04:50 +0100

    12- avancée 80% sur page logement + style et function

---
+++ src/components/BannerCarrousel.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BannerCarrousel = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default BannerCarrousel;
--- src/pages/FicheLogement.jsx
+++ src/pages/FicheLogement.jsx
@@ -1,15 +1,14 @@
 // import React, { useEffect, useState } from 'react';
 // import { useLocation } from 'react-router-dom';
 
-
 // function FicheLogement() {
 //   const location = useLocation()
 //   console.log(location)
 //   console.log(location.state.cardId)
 //   const [selectedCard, setSelectedCard] = useState(null);
-  
+
 //   useEffect(getDatasById, []);
-  
+
 //   function getDatasById() {
 //     fetch('/datas.json')
 //       .then((res) => res.json())
@@ -18,44 +17,99 @@
 //           console.log(foundCard)
 //           setSelectedCard(card)
 //         })
-      
+
 //       .catch((err) => console.log(err))
 
 //   }
 //   return (
 //     <div>
-      
+
 //     </div>
 //   );
 // }
 
 // export default FicheLogement;
 
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import Collapse from "../components/Collapse";
+import "../styles/ficheLogement.css";
 
 function FicheLogement() {
   const { id } = useParams();
   const [selectedCard, setSelectedCard] = useState(null);
-  
+
   useEffect(() => {
-    axios.get('/datas.json')
-      .then(response => {
-        const foundCard = response.data.find(card => card.id === id);
+    axios
+      .get("/datas.json")
+      .then((response) => {
+        const foundCard = response.data.find((card) => card.id === id);
         setSelectedCard(foundCard);
       })
-      .catch(error => console.log(error));
+      .catch((error) => console.log(error));
   }, [id]);
-  
+
   return (
     <main>
       {selectedCard && (
-        <div>
-          <h2>{selectedCard.title}</h2>
-          <img src={selectedCard.cover} alt={selectedCard.title} />
-          <p>{selectedCard.description}</p>
-        </div>
+        <>
+          <section id="bannerCarrousel">
+            <banner>
+              <img src={selectedCard.cover} alt={selectedCard.title} />
+            </banner>
+          </section>
+
+          <section id="presentation">
+            <div className="presentation">
+              <h2 className="titleLogement">{selectedCard.title}</h2>
+              <h3>{selectedCard.location}</h3>
+              <div className="tagsContainer">
+                {selectedCard.tags.map((tag, index) => (
+                  <span key={index} className="tags">
+                    {tag}
+                  </span>
+                ))}
+              </div>
+            </div>
+            <div id="author">
+              <div className="author">
+                <p>{selectedCard.host.name}</p>
+                <div className="roundImg">
+                  <img
+                    src={selectedCard.host.picture}
+                    alt="photographie de l'auteur"
+                  />
+                </div>
+              </div>
+              <div className="rating">
+                <span>★</span>
+                <span>★</span>
+                <span>★</span>
+                <span>★</span>
+                <span>★</span>
+              </div>
+            </div>
+          </section>
+
+          <section id="description">
+            <div className="collapses">
+              <div className="details">
+                <Collapse title="Description">
+                  <p>{selectedCard.description}</p>
+                </Collapse>
+              </div>
+
+              <div className="details">
+                <Collapse title="Equipements">
+                  {selectedCard.equipments.map((equipment, index) => (
+                    <p key={index}>{equipment}</p>
+                  ))}
+                </Collapse>
+              </div>
+            </div>
+          </section>
+        </>
       )}
     </main>
   );
