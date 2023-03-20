d24712a0e54876b1a65f8e4b672fcbd688593764
YoannPitance <pittyo88@gmail.com>
2023-03-20 08:46:06 +0100

    13- modification page logement pour redirection 404 si id inconnu

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,15 @@
-import React from 'react';
-import {Link} from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Card(props) {
-    
-    
-    return (    
-        <Link to={`/fiche-logement/${props.id}`} className='card'>
-            <div className='cardImage'>
-                <img src={props.cover} alt={props.title}/>
-            </div>
-            <div className='cardText'>
-                <h2>{props.title}</h2>
-            </div>
-        </Link>       
-    );
-};
+  return (
+    <Link to={`/fiche-logement/${props.id}`} className="card">
+      <div className="cardImage">
+        <img src={props.cover} alt={props.title} />
+        <h2 className="cardText">{props.title}</h2>
+      </div>
+    </Link>
+  );
+}
 
 export default Card;
--- src/pages/FicheLogement.jsx
+++ src/pages/FicheLogement.jsx
@@ -35,29 +35,47 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import Collapse from "../components/Collapse";
 import "../styles/ficheLogement.css";
+import Page404 from "../pages/Page404";
 
 function FicheLogement() {
   const { id } = useParams();
   const [selectedCard, setSelectedCard] = useState(null);
+  const [notFound, setNotFound] = useState(false);
 
   useEffect(() => {
     axios
       .get("/datas.json")
       .then((response) => {
-        const foundCard = response.data.find((card) => card.id === id);
-        setSelectedCard(foundCard);
+        const exists = response.data.some((card) => card.id === id);
+        if (exists) {
+          const foundCard = response.data.find((card) => card.id === id);
+          setSelectedCard(foundCard);
+        } else {
+          setNotFound(true);
+        }
       })
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        console.log(error);
+        setNotFound(true);
+      });
   }, [id]);
+
+  if (notFound) {
+    return <Page404 />;
+  }
+
+  if (!selectedCard) {
+    return null;
+  }
 
   return (
     <main>
       {selectedCard && (
         <>
           <section id="bannerCarrousel">
-            <banner>
-              <img src={selectedCard.cover} alt={selectedCard.title} />
-            </banner>
+            <div className="banner">
+              <img src={selectedCard.pictures[0]} alt={selectedCard.title} />
+            </div>
           </section>
 
           <section id="presentation">
